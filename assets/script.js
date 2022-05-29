b3ebc823b7c9c1fe0a6b1d8d0dd73c0fc4638ae7
Adarsh <adarshbhardwaj285@gmail.com>
2022-05-29 18:10:58 +0530

    Countries list is Added

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 const month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 const day =['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 let body = document.querySelector('body');
+const country = countries;
 let showAddNewCountry = document.querySelectorAll('[data-btn="show-add-new"]');
 let showCountryElement = document.querySelector('[data-show="search-menu"]');
 let CurrentTimeElement = document.querySelector('[data-time="current-time"]');
+let CityList = document.querySelector('[data-city="list"]');
+/* ------ Current Date and Time Start ------ */
 function showTime()
 {
     const date = new Date();
@@ -27,7 +30,6 @@ function showTime()
     CurrentTimeElement.textContent = `${h}:${m}:${s} ${meridies}`;
     s++;
 }
-
 function SetDate()
 {
     const clock = new Date();
@@ -37,8 +39,11 @@ function SetDate()
     let dayCount = clock.getDay();
     document.querySelector('[data-time="current-date"]').innerHTML = `${day[dayCount]}, ${date} ${month[monthCount]} ${year}`;
 }
+/* ------ Current Date and Time End ------ */
 SetDate();
 setInterval(showTime,1000);
+
+/* ------ Add New Country Model Start ------ */
 function showAdd(){
     showCountryElement.style.display ="block"
     body.style.backgroundColor = "rgba(0,0,0,0.5)"
@@ -50,12 +55,58 @@ function closeSearch()
     body.style.backgroundColor = "white";
 }
 
-let searchCountry = document.querySelector('[data-search="country"]');
-
-let country = countries;
-
-country.forEach(function(country){
-    let option = document.createElement('option');
-    option.textContent = country.capital+ ','+country.name;
-    searchCountry.appendChild(option);
+// function searchCity(value)
+// {
+//     console.log(value);
+// }
+//Sort Object By Capital Name
+country.sort((a,b)=>{
+    return a.capital.localeCompare(b.capital);
 });
+
+// country.forEach(element => {
+//     console.log(element.capital+', '+element.name);
+// });
+function ShowAllTimeZone()
+{
+    country.forEach(element => {
+        if(element.capital == '')
+        {
+            let newElement = document.createElement('div');
+            newElement.setAttribute('onclick','addCity(this)');
+            newElement.classList.add('cities-items');
+            newElement.innerHTML =`<h3>${element.name}</h3>`
+            CityList.append(newElement);
+        }
+        else{
+            let newElement = document.createElement('div');
+            newElement.setAttribute('onclick','addCity(this)');
+            newElement.classList.add('cities-items');
+            newElement.innerHTML =`<h3>${element.capital}, ${element.name} </h3>`
+            CityList.append(newElement);
+        }
+    });
+    newElement='';
+}
+ShowAllTimeZone();
+
+function addCity(val)
+{
+    let date= new Date();
+    let index=0;
+    let countryArr = val.textContent.split(',');
+    console.log(countryArr[0]);
+    if(countryArr.length == 1)
+    {
+        let index = country.map(element=>element.name).indexOf(countryArr[0]);
+        console.log(index);
+        console.log(country[index].timezones[0]);
+    }
+    else{
+        let index = country.map(element=>element.capital).indexOf(countryArr[0]);
+        console.log(index);
+        console.log(country[index].timezones[0]);
+    }
+    console.log(date.toLocaleString('en-US', { timeZone: country[index].timezones[0] }));
+
+}
